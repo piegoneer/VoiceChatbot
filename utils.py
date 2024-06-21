0f7b63a14ffb4a499f61ba06ba697d2c3d2bdc87
Hugh Bisset <hugh.bisset@gmail.com>
2024-06-21 15:23:11 +1200

    simple TTA test, ability to convert text to audio file

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import os
+from pathlib import Path
+import openai
 
 configPathLocation = "config.txt"
+client = None
 
 def readConfig(file_path):
     config = {}
@@ -14,4 +17,24 @@ def getOPENAI_API_KEY():
     config = readConfig(configPathLocation)
     OPENAI_API_KEY = config.get('OPENAI_API_KEY')
     return OPENAI_API_KEY
+
+def setupOpenAIAPI():
+    global client
+    OPENAI_API_KEY = getOPENAI_API_KEY()
+    client = openai.OpenAI(api_key=OPENAI_API_KEY)
+
+
+def textToAudioFile(text, audioFileOuput):
+    speech_file_path = audioFileOuput
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input=text
+    )
+
+    response.stream_to_file(speech_file_path)
+    
+
+
+
     
